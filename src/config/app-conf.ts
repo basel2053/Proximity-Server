a324d15da755525d42a1a8c8bb943a274d4139d6
Bassel Salah <baselsalah2053@gmail.com>
2023-10-01 17:35:02 +0300

    refactor: app config

--- src/config/app-conf.ts
+++ src/config/app-conf.ts
@@ -8,14 +8,40 @@ import MongoStore from 'connect-mongo';
 
 import intializeStrategies from '../auth/strategies';
 import passport from 'passport';
-// import swaggerUI from 'swagger-ui-express'
+import swaggerUI from 'swagger-ui-express';
+import swaggerJsdoc from 'swagger-jsdoc';
 
-// import swaggerDocument from '../../swagger.json';
+const swaggerOptions = {
+  definition: {
+    openapi: '3.1.0',
+    info: {
+      version: '1.0.0',
+      title: 'Robust Chat API',
+      description: 'This is a simple API for chat application made with Express and documented with Swagger',
+      license: {
+        name: 'MIT',
+        url: 'https://spdx.org/licenses/MIT.html',
+      },
+      contact: {
+        name: 'Schat',
+        url: 'http://localhost:3000.com',
+        email: 'test@test.com',
+      },
+    },
+    servers: [
+      {
+        url: 'http://localhost:3000',
+      },
+    ],
+  },
+  apis: ['../routes.ts'],
+};
 
 export default class ApplicationConfig {
   static init(app: express.Application): void {
     const { SESSION_SECRET, MONGO_URI } = process.env;
     const sessionStore = MongoStore.create({ mongoUrl: MONGO_URI, collectionName: 'sessions' });
+    const specs = swaggerJsdoc(swaggerOptions);
     app.set('view engine', 'ejs');
     app.use(cors());
     app.use(express.json());
@@ -24,6 +50,9 @@ export default class ApplicationConfig {
     app.use(helmet());
     app.use(hpp());
     app.use(express.static('public'));
+    app.use('/api-docs', (req, res) => {
+      res.send('heya');
+    });
     app.use(
       session({
         secret: String(SESSION_SECRET),
@@ -35,6 +64,7 @@ export default class ApplicationConfig {
     app.use(passport.initialize());
     app.use(passport.session());
     intializeStrategies();
-    // app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+
+    app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(specs));
   }
 }
